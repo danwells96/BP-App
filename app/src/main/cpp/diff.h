3dba411ad0909470283ae9abc1d8a1f4618bf509
PauloIswanto <PauloIswanto@users.noreply.github.com>
2018-06-16 15:52:40 +0100

    Add JNI support.

---
+++ app/src/main/cpp/diff.h
@@ -0,0 +1,27 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * diff.h
+ *
+ * Code generation for function 'diff'
+ *
+ */
+
+#ifndef BPAPP_DIFF_H
+#define BPAPP_DIFF_H
+
+/* Include files */
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rtwtypes.h"
+#include "wabp_types.h"
+
+/* Function Declarations */
+extern void diff(const double x[97], double y[96]);
+
+#endif //BPAPP_DIFF_H
+
+/* End of code generation (diff.h) */
