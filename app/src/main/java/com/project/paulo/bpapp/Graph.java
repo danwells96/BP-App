e0a362cc7e77ed049485c18230dd747733fdbf81
PauloIswanto <PauloIswanto@users.noreply.github.com>
2018-06-16 13:10:58 +0100

    Add y-axis title.

--- app/src/main/java/com/project/paulo/bpapp/Graph.java
+++ app/src/main/java/com/project/paulo/bpapp/Graph.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
