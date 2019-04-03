9464b3527c02235468a6c3900c912945f23a35b3
Dan Wells <dtw14@ic.ac.uk>
2019-04-03 16:23:06 +0100

    Fixed bug where dialog couldnt find resources in layout

--- app/src/main/java/com/project/paulo/bpapp/ReaderDialog.java
+++ app/src/main/java/com/project/paulo/bpapp/ReaderDialog.java
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 public class ReaderDialog extends DialogFragment {
     @NonNull
@@ -14,12 +16,15 @@ public class ReaderDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
+        View layout = inflater.inflate(R.layout.popup_readerparams, null);
 
-        builder.setView(R.layout.popup_readerparams);
+        final EditText activeFrequency = layout.findViewById(R.id.edit_active_frequency);
+        builder.setView(layout);
         builder.setPositiveButton("Save Values", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                String x = activeFrequency.getText().toString();
+                System.out.println(x);
             }
         });
 
